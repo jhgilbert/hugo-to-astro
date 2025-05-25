8ccdf2a000d5e6684938a849d0ebeb6574d21fe7
Jennifer Gilbert <j.h.gilbert@gmail.com>
2025-05-24 19:23:17 -0500

    Sketch in migration function

--- h2a-migrator/src/index.ts
+++ h2a-migrator/src/index.ts
@@ -1,1 +1,9 @@
-console.log("Hello, world!");
+// Not really how we would typically do this, but it's not that serious
+const HUGO_SITE_DIR = __dirname + "../../example_hugo_site";
+const ASTRO_SITE_DIR = __dirname + "../../example_astro_site";
+
+function migrateContent() {
+  console.log("Migrating content from Hugo to Astro...");
+}
+
+migrateContent();
